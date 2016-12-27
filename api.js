f403f060fbd9167cfccd57ae4f9d1217a228c74f
Sébastien Chopin <seb@chopin.io>
2016-12-27 23:10:37 +0100

    Split api and gh-hook

--- api.js
+++ api.js
@@ -1,20 +1,14 @@
 const fs = require('fs')
-const crypto = require('crypto')
-const os = require('os')
-const git = require('simple-git')()
-const uuid = require('uuid/v4')
 const micro = require('micro')
 const pify = require('pify')
-const rimraf = pify(require('rimraf'))
 const glob = pify(require('glob'))
 const marked = require('marked')
 const highlightjs = require('highlight.js')
 const fm = require('front-matter')
 const { resolve } = require('path')
+const githubHook = require('./gh-hook')
 const readFile = pify(fs.readFile)
-const gitClone = pify(git.clone.bind(git))
 const send = micro.send
-const json = micro.json
 
 // Use highlight.js for code blocks
 const renderer = new marked.Renderer()
@@ -69,42 +63,11 @@ async function getDocFile (path) {
   return _CACHE_DOC_FILES_[path]
 }
 
-// Refresh documentation files (pull from Github)
-async function pullDocFiles ({ req, res }) {
-  const body = await json(req)
-  console.log(body)
-  // Only for production
-  if (!process.env.GH_HOOK_SECRET || !req.headers['x-hub-signature']) {
-    return send(res, 501)
-  }
-  // Check if X-Hub-Signature matches our secret
-  let hmac = crypto.createHmac('sha1', process.env.GH_HOOK_SECRET)
-  hmac.update(JSON.stringify(body))
-  let signature = 'sha1=' + hmac.digest('hex')
-  if (req.headers['x-hub-signature'] !== signature) {
-    return send(res, 403)
-  }
-  // Accept only push hook events
-  if (req.headers['x-github-event'] === 'ping') {
-    return send(res, 200, 'OK')
-  }
-  // Only push event authorized
-  if (req.headers['x-github-event'] !== 'push') {
-    return send(res, 501)
-  }
-  const clonePath = resolve(os.tmpdir(), uuid())
-  await gitClone('https://github.com/nuxt/docs.git', clonePath)
-  console.log('Cloned', clonePath)
-  await getFiles()
-  await rimraf(clonePath)
-  send(res, 200, 'OK')
-}
-
 // Server handle request method
 const server = micro(async function (req, res) {
   // If github hook
   if (req.method === 'POST' && req.url === '/hook') {
-    return await pullDocFiles({ req, res })
+    return await githubHook({ req, res }, getFiles)
   }
   if (req.url === '/menu') {
     return send(res, 200, _CACHE_MENU_)
---
+++ gh-hook.js
@@ -0,0 +1,45 @@
+const micro = require('micro')
+const crypto = require('crypto')
+const os = require('os')
+const git = require('simple-git')()
+const uuid = require('uuid/v4')
+const pify = require('pify')
+const rimraf = pify(require('rimraf'))
+let mergeDirs = require('merge-dirs')
+const gitClone = pify(git.clone.bind(git))
+const { resolve } = require('path')
+const json = micro.json
+const send = micro.send
+// Fix merge-dirs default export
+mergeDirs = mergeDirs.default ? mergeDirs.default : mergeDirs
+
+// Refresh documentation files (pull from Github)
+module.exports = async function ({ req, res }, getFiles) {
+  const body = await json(req)
+  console.log(body)
+  // Only for production
+  if (!process.env.GH_HOOK_SECRET || !req.headers['x-hub-signature']) {
+    return send(res, 501)
+  }
+  // Check if X-Hub-Signature matches our secret
+  let hmac = crypto.createHmac('sha1', process.env.GH_HOOK_SECRET)
+  hmac.update(JSON.stringify(body))
+  let signature = 'sha1=' + hmac.digest('hex')
+  if (req.headers['x-hub-signature'] !== signature) {
+    return send(res, 403)
+  }
+  // Accept only push hook events
+  if (req.headers['x-github-event'] === 'ping') {
+    return send(res, 200, 'OK')
+  }
+  // Only push event authorized
+  if (req.headers['x-github-event'] !== 'push') {
+    return send(res, 501)
+  }
+  const clonePath = resolve(os.tmpdir(), uuid())
+  await gitClone('https://github.com/nuxt/docs.git', clonePath)
+  mergeDirs(clonePath, resolve(__dirname, 'test'), 'overwrite')
+  await getFiles()
+  await rimraf(clonePath)
+  send(res, 200, 'OK')
+}
